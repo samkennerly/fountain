c7263eacf60b320fced2f1356c2633bcc4fa4784
Sam Kennerly <samkennerly@gmail.com>
2019-11-26 13:34:27 -0500

    Move fountain.py outside pydiner package

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,11 @@
 """
-Test frameworks might need this file to discover tests.
+Repackage 1-line test for compatibility with various testing frameworks.
 """
+import doctest
+
+import fountain
+
+def test_fountain():
+    """ None: Raise error if any doctests failed. """
+    assert not doctest.testmod(module).failed
+    print("OK fountain")
--- test/__main__.py
+++ test/__main__.py
@@ -1,21 +1,6 @@
 """
-Example: Discover and run tests in each module.
+Run tests with import hacks for compatibility with various testing frameworks.
 """
-from . import fixtures, test_fountain, test_utensils
+from test import test_fountain
 
-
-def test_all(module):
-    print("", module.__name__, 10 * "-", sep="\n")
-    for name, meth in vars(module).items():
-        if name.startswith("test_"):
-            print(name)
-            meth()
-
-
-print("Run", __file__)
-
-test_all(test_fountain)
-test_all(test_utensils)
-fixtures.cleartmp()
-
-print("\nExit", __file__)
+test_fountain()
